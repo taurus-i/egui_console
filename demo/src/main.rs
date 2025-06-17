a6e4e0fbdc864f9550b49d771fa2717f824d3d64
buffalo <nobody@nothing.com>
2025-06-17 13:04:04 +0800

    update

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,6 +1,10 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 #![allow(missing_docs)]
+
+#[cfg(target_arch = "wasm32")]
+use web_sys::wasm_bindgen::JsCast;
+
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result {
@@ -35,7 +39,11 @@ fn main() {
     wasm_bindgen_futures::spawn_local(async {
         let start_result = eframe::WebRunner::new()
             .start(
-                "the_canvas_id",
+                web_sys::window()
+                    .and_then(|w| w.document())
+                    .and_then(|d| d.get_element_by_id("the_canvas_id"))
+                    .and_then(|e| e.dyn_into::<web_sys::HtmlCanvasElement>().ok())
+                    .expect("Failed to get canvas element"),
                 web_options,
                 Box::new(|cc| Ok(Box::new(demo::ConsoleDemo::new(cc)))),
             )
